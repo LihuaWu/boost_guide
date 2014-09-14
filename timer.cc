b9924cc3188ad33ac675b3d0aeb9de9291641485
LihuaWu <wulihua0@gmail.com>
2014-09-14 21:48:45 +0800

    first push

--- timer.cc
+++ timer.cc
@@ -1,9 +1,31 @@
 #include <boost/timer.hpp>
+#include <boost/progress.hpp>
+
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
 
 using namespace boost;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::ofstream;
+using std::string;
+
 int main() {
-	timer t;	
-	std::cout<<"max timespan:"<<t.elapsed_max()<<std::endl;
+	//timer t;	
+	//progress_timer m;
+	//std::cout<<"max timespan:"<<t.elapsed_max()<<std::endl;
+
+	ofstream fs("test.in");
+	string s("ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss");
+	vector<string> v(100*100*100, s);
+	progress_display pd(v.size());
+	vector<string>::iterator pos;
+	for(pos = v.begin();  pos != v.end(); ++pos) {
+		fs<< *pos << endl;
+		++pd;
+	}
 	return 0;
 }
